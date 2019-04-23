3f2a9a4da43ef1279cdb13eecae7a9122aec86ae
Howard Wang <44262420+MirstT@users.noreply.github.com>
2019-04-23 19:31:18 +0800

    Update Model.py

--- week5/Model.py
+++ week5/Model.py
@@ -1,7 +1,21 @@
+# 创建tensor
+print("正在创建模型...")
+inputs=Input(shape=(sequence_length,),dtype='int32')
+embedding=Embedding(input_dim=vocabulary_size,output_dim=embedding_dim,input_length=sequence_length)(inputs)
+reshape=Reshape((sequence_length,embedding_dim,1))(embedding)
 
-print('正在创建模型')
-inputs = Input(shape=(sequence_length,),dtype='int32')
-embedding = Embedding(input_dim=vocabulary_size,output_dim=embedding_dim,input_length=sequence_lengrh)(inputs)
-reshape = Reshape((sequence_length,embedding_dim,1))(embedding)
+# cnn
+conv_0=Conv2D(num_filters,kernel_size=(filter_sizes[0],embedding_dim),padding='valid',kernel_initializer='normal',activation='relu')(reshape)
+conv_1=Conv2D(num_filters,kernel_size=(filter_sizes[1],embedding_dim),padding='valid',kernel_initializer='normal',activation='relu')(reshape)
+conv_2=Conv2D(num_filters,kernel_size=(filter_sizes[2],embedding_dim),padding='valid',kernel_initializer='normal',activation='relu')(reshape)
 
-conv_0 = Conv2D(num_filters,kernel_size=(filter_size[0],embedding_dim),padding='valid',kernel_ini
+maxpool_0=MaxPool2D(pool_size=(sequence_length-filter_sizes[0]+1,1),strides=(1,1),padding='valid')(conv_0)
+maxpool_1=MaxPool2D(pool_size=(sequence_length-filter_sizes[1]+1,1),strides=(1,1),padding='valid')(conv_1)
+maxpool_2=MaxPool2D(pool_size=(sequence_length-filter_sizes[2]+1,1),strides=(1,1),padding='valid')(conv_2)
+
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
+dropout = Dropout(drop)(flatten)
+output = Dense(units=2, activation='softmax')(dropout)
+model=Model(inputs=inputs,outputs=output)
+
